cf693cf01cd1b528d704f0601ef11335b308d142
JeremyGamer13 <69337718+JeremyGamer13@users.noreply.github.com>
2025-07-25 19:26:48 -0600

    a lot of these quick replys are outdated

--- src/commands/multi/quickcmds.js
+++ src/commands/multi/quickcmds.js
@@ -5,7 +5,8 @@ class ScratchAuthCommand extends QuickReplyCommand {
         super();
         this.name = "scratchauth";
         this.description = "ScratchAuth Quick Reply";
-        this.message = "If you have errors logging in, it is most likely an issue on Scratch Auth's end. Scratch Auth is not affiliated with PenguinMod, however we are working on our own login method. Please note that this will take time, so please have patience. We apologize for the inconvenience.";
+        this.message = "If you have errors logging in using Scratch, it is most likely an issue with Scratch Wiki OAuth2. We use a login method provided by part of the Scratch Wiki since it is pretty reliable, but it might go down every now and then. When you are able to login with Scratch, try adding another login method or setting a password for your account.";
+        this.alias = ["scratchlogin"];
     }
 }
 class DesktopCommand extends QuickReplyCommand {
@@ -13,7 +14,7 @@ class DesktopCommand extends QuickReplyCommand {
         super();
         this.name = "desktop";
         this.description = "Desktop Quick Reply";
-        this.message = "It's in development already. The application will allow you to use the home page & editor, open project files in your file explorer app, and get new updates or revert to a previous update.";
+        this.message = "The PenguinMod Desktop app is a planned project the PenguinMod developers want to make soon. It is not a thing yet.\nThe best third-party alternative for now is an unofficial PenguinMod Desktop app named \"Desk-Penguin\" made by TheShovel.\n<https://github.com/TheShovel/Desk-Penguin> (this is an unofficial project, not affiliated with PenguinMod.)";
     }
 }
 class RankupCommand extends QuickReplyCommand {
@@ -30,7 +31,8 @@ class LoadSB3Command extends QuickReplyCommand {
         this.name = "loadsb3";
         this.description = "Load .sb3 Quick Reply";
         this.message = "Want to load your TurboWarp or Scratch project into PenguinMod?\n"
-            + "Try this tutorial! https://discord.com/channels/1033551490331197462/1181430242963902565";
+            + "Try this tutorial! https://discord.com/channels/1033551490331197462/1181430242963902565\n"
+            + "Note that PenguinMod may be incompatible with Custom Block reporters or complicated custom extensions.";
     }
 }
 class CollabCommand extends QuickReplyCommand {
@@ -38,7 +40,7 @@ class CollabCommand extends QuickReplyCommand {
         super();
         this.name = "collab";
         this.description = "Collab/Blocklive Quick Reply";
-        this.message = "This is a cool idea, it really is. But we currently don't know if this would be possible to implement without tons of time or money. Please be patient with us on adding this.";
+        this.message = "A collaboration system for projects is a cool idea, but we currently don't know if this would be possible to implement without tons of time or money.";
         this.alias = ["blocklive"];
     }
 }
@@ -47,7 +49,7 @@ class CommentCommand extends QuickReplyCommand {
         super();
         this.name = "comment";
         this.description = "Comment Quick Reply";
-        this.message = "We are looking into this, but we are spending a lot of time looking for a good way to moderate the comments to make sure the platform is safe for all ages.";
+        this.message = "We have not added comments to PenguinMod since there is not a reliable, safe and cheap way to moderate the comments to make sure the platform is safe for all ages.";
     }
 }
 class StudioCommand extends QuickReplyCommand {
@@ -55,7 +57,7 @@ class StudioCommand extends QuickReplyCommand {
         super();
         this.name = "studio";
         this.description = "Studio Quick Reply";
-        this.message = "This would be nice to have, but we are currently busy updating the uploading server that handles projects to be a lot faster and better. Once this update is done, we may be able to add studios.";
+        this.message = "Studios would be a cool feature to add to PenguinMod, but we have a lot more important things to add first. They will hopefully be added eventually, but don't expect or ask for a specific timeframe right now.";
     }
 }
 class ServerCommand extends QuickReplyCommand {
@@ -63,7 +65,7 @@ class ServerCommand extends QuickReplyCommand {
         super();
         this.name = "server";
         this.description = "Server Quick Reply";
-        this.message = "It's likely we are having some trouble with services connected to the server. Downtime will be mentioned in <#1040401867924062259> if major reasons are causing downtime.";
+        this.message = "If you are having issues with the PenguinMod servers, it's likely there is a small issue causing the servers to be unavailable. Downtime will be mentioned in <#1040401867924062259> if major reasons are causing downtime.";
         this.alias = ["serverdown"];
     }
 }
@@ -72,7 +74,8 @@ class SHCommand extends QuickReplyCommand {
         super();
         this.name = "scratchhelp";
         this.description = "Scratch Help Quick Reply";
-        this.message = "This server will not help you with scratch projects! Go to https://discord.gg/7gfPrhWwb2 instead.";
+        this.message = "This server is intended to help you with PenguinMod projects. If you specifically need Scratch help, check out other Scratch servers like <https://discord.gg/7gfPrhWwb2> (Scratch Community) or go on the Scratch forums.";
+        this.alias = ["scratchelp"];
     }
 }
 class GoochCommand extends QuickReplyCommand {
@@ -81,6 +84,14 @@ class GoochCommand extends QuickReplyCommand {
         this.name = "gooch";
         this.description = "Who was here?";
         this.message = "Sir William Gooch was here.";
+    }
+}
+class MyStuffCommand extends QuickReplyCommand {
+    constructor() {
+        super();
+        this.name = "mystuff";
+        this.description = "My Stuff Quick Reply";
+        this.message = "There is no way to save in-progress projects to the server in PenguinMod. Unlike Scratch, we don't have the resources to do something like this. We only have a limited amount of storage space and we can't dedicate it to hosting thousands of unfinished projects safely. We also don't want to get in the way of working on your projects if the servers ever go down for a long amount of time.";
     }
 }
 
@@ -95,5 +106,6 @@ module.exports = {
     studio: StudioCommand,
     server: ServerCommand,
     scratchhelp: SHCommand,
-    gooch: GoochCommand
+    gooch: GoochCommand,
+    mystuff: MyStuffCommand,
 };
